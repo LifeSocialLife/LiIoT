cab3949be025c0b8f1fb63aa8bb42984689966a6
Lennie Wennerlund <lennie@lempa.se>
2021-07-11 11:58:04 +0200

    Issue done close #4

---
+++ src/LiIoT.Models/Rundata/RundataServiceHardwareModel.cs
@@ -0,0 +1,73 @@
+﻿// <summary>
+// Storage pool work.
+// </summary>
+// <copyright file="RundataServiceHardwareModel.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
+// <author>Lennie Wennerlund (lempa)</author>
+
+namespace LiIoT.Models.Rundata
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using static LiTools.Helpers.IO.Hardware;
+
+    /// <summary>
+    /// Rundata Service Hardware model.
+    /// </summary>
+    public class RundataServiceHardwareModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RundataServiceHardwareModel"/> class.
+        /// </summary>
+        public RundataServiceHardwareModel()
+        {
+            this.Os = PlatformOsEnum.None;
+            this.FrameworkDescription = string.Empty;
+            this.OSArchitecture = ArchitectureEnum.none;
+            this.ProcessorArchitecture = ArchitectureEnum.none;
+            this.OsDescription = string.Empty;
+        }
+
+        /// <summary>
+        /// Gets or sets os this is running on. Windows, linux, osv.
+        /// </summary>
+        public PlatformOsEnum Os { get; set; }
+
+        /// <summary>
+        /// Gets or sets what is the os architecture. x86 or x64 ??.
+        /// </summary>
+        public ArchitectureEnum OSArchitecture { get; set; }
+
+        /// <summary>
+        /// Gets or sets os Description. this contains os version if running on windows.
+        /// </summary>
+        public string OsDescription { get; set; }
+
+        /// <summary>
+        /// Gets or sets framework information. dotnet framwork information.
+        /// </summary>
+        public string FrameworkDescription { get; set; }
+
+        /// <summary>
+        /// Gets or sets what is the CPU (processor) architecure. X86 or X64 ??.
+        /// </summary>
+        public ArchitectureEnum ProcessorArchitecture { get; set; }
+
+        /// <summary>
+        /// Get all data from this model as dict.
+        /// </summary>
+        /// <returns>dict modelname and value.</returns>
+        public Dictionary<string, string> GetAllData()
+        {
+            var aa = new Dictionary<string, string>();
+            aa.Add("Os", this.Os.ToString());
+            aa.Add("OSArchitecture", this.OSArchitecture.ToString());
+            aa.Add("OsDescription", this.OsDescription);
+            aa.Add("FrameworkDescription", this.FrameworkDescription);
+            aa.Add("ProcesArchitecture", this.ProcessorArchitecture.ToString());
+            return aa;
+        }
+    }
+}
--- src/StarterNoGui/Worker.cs
+++ src/StarterNoGui/Worker.cs
@@ -61,12 +61,33 @@ namespace StarterNoGui
             this._hostApplicationLifetime.ApplicationStopping.Register(this.OnStopping, true);
             this._hostApplicationLifetime.ApplicationStopped.Register(this.OnStopped, true);
 
-            var d2 = this._configuration.GetValue<string>(WebHostDefaults.ContentRootKey);
-
             this.zzDebug = "sdfdf";
+
+            this._rundata.StartUpRunningStage = 1;
 
             while (!stoppingToken.IsCancellationRequested)
             {
+                // Do we have a error in software that is stopping it from starting?.
+                if (this._rundata.StartUpRunningPart == StartUpRunningPartEnum.Error)
+                {
+                    await Task.Delay(5000, stoppingToken);
+                    continue;
+                }
+
+                // Run Stage is below 1000. software is in startupScheduler.
+                if (this._rundata.StartUpRunningStage < 1000)
+                {
+                    if (await this.RunUpstartScheduler())
+                    {
+                    }
+
+                    continue;
+                }
+
+                var a1 = this._rundata.Folders;
+                var a2 = this._rundata.Hardware;
+                this.zzDebug = "SDfdsf";
+
                 this._logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 try
                 {
@@ -77,6 +98,66 @@ namespace StarterNoGui
                     return;
                 }
             }
+        }
+
+        private async Task<bool> RunUpstartScheduler()
+        {
+            if (this._rundata.StartUpRunningStage < 10)
+            {
+                this._rundata.StartUpRunningPart = StartUpRunningPartEnum.Init;
+            }
+
+            // Shod we run stage 1 - Get Folder inforamtion.
+            if (this._rundata.StartUpRunningStage == 1)
+            {
+                if (this.GetRundataFolderData())
+                {
+                    await Task.Delay(1000);
+                    this._rundata.StartUpRunningStage = 2;
+                }
+                else
+                {
+                    this._rundata.StartUpRunningPart = StartUpRunningPartEnum.Error;
+                    return false;
+                }
+            }
+
+            // Shod we run stage 2 - Get hardware inforamtion.
+            if (this._rundata.StartUpRunningStage == 2)
+            {
+                if (this._rundata.SetHardwareModel())
+                {
+                    await Task.Delay(1000);
+                    this._rundata.StartUpRunningStage = 3;
+                }
+                else
+                {
+                    this._rundata.StartUpRunningPart = StartUpRunningPartEnum.Error;
+                    return false;
+                }
+            }
+
+            this._rundata.StartUpRunningStage = 1000;
+            return true;
+        }
+
+        private bool GetRundataFolderData()
+        {
+            this._rundata.Folders.PathRuntimes = this._configuration.GetValue<string>(WebHostDefaults.ContentRootKey);
+
+#pragma warning disable CS8601 // Possible null reference assignment.
+            if (System.Diagnostics.Process.GetCurrentProcess()?.MainModule?.FileName != null)
+            {
+                this._rundata.Folders.PathExecuteFile = System.Diagnostics.Process.GetCurrentProcess()?.MainModule?.FileName;
+            }
+
+            if (this._rundata.Folders.PathExecuteFile != string.Empty)
+            {
+                this._rundata.Folders.PathExecute = System.IO.Path.GetDirectoryName(this._rundata.Folders.PathExecuteFile);
+            }
+#pragma warning restore CS8601 // Possible null reference assignment.
+
+            return true;
         }
 
         private void OnStarted()
