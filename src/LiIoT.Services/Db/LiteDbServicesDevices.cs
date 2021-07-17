13b9b4e3d71954388ff87aaa33fca86f9959132a
Lennie Wennerlund <lennie@lempa.se>
2021-07-17 13:37:25 +0200

    Issue doen. close #19

--- src/LiIoT.Services/Db/LiteDbServicesDevices.cs
+++ src/LiIoT.Services/Db/LiteDbServicesDevices.cs
@@ -17,6 +17,7 @@ namespace LiIoT.Services.Db
     using System.Diagnostics.CodeAnalysis;
     using System.Text;
     using System.Threading.Tasks;
+    using LiIoT.Models.DbLite;
     using LiteDB;
     using LiteDB.Async;
     using Microsoft.Extensions.Logging;
@@ -32,9 +33,7 @@ namespace LiIoT.Services.Db
         private readonly LiteDbService _db;
 #pragma warning restore SA1309 // FieldNamesMustNotBeginWithUnderscore
 
-        private LiteCollectionAsync<DevicesModel> col;
-
-        private bool InitIsDone { get; set; }
+        private LiteCollectionAsync<DbDeviceModel> col;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="LiteDbServicesDevices"/> class.
@@ -42,7 +41,9 @@ namespace LiIoT.Services.Db
         /// <param name="logger">ILogger.</param>
         /// <param name="runDataService">RunDataService.</param>
         /// <param name="liteDbService">LiteDbService.</param>
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public LiteDbServicesDevices(ILogger<LiteDbServicesDevices> logger, RunDataService runDataService, LiteDbService liteDbService)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
             this._logger = logger;
             this._rundata = runDataService;
@@ -51,21 +52,21 @@ namespace LiIoT.Services.Db
             this.InitIsDone = false;
         }
 
+        private bool InitIsDone { get; set; }
+
         [SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "Reviewed.")]
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Reviewed.")]
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
         private string zzDebug { get; set; }
 
-        
-
         /// <summary>
         /// Get all devices.
         /// </summary>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-        public async Task<Tuple<bool, List<DevicesModel>>> GetAll()
+        public async Task<Tuple<bool, List<DbDeviceModel>>> GetAll()
         {
             this.Init();
-            var aa = new List<DevicesModel>();
+            var aa = new List<DbDeviceModel>();
             var error = false;
             this._db._lock.WaitOne(-1);
 
@@ -76,6 +77,8 @@ namespace LiIoT.Services.Db
             catch (Exception e)
             {
                 error = true;
+                this._logger.LogWarning(e.Message);
+
                 if (System.Diagnostics.Debugger.IsAttached)
                 {
                     System.Diagnostics.Debugger.Break();
@@ -91,17 +94,17 @@ namespace LiIoT.Services.Db
 
             if (error)
             {
-                return new Tuple<bool, List<DevicesModel>>(false, new List<DevicesModel>());
+                return new Tuple<bool, List<DbDeviceModel>>(false, new List<DbDeviceModel>());
             }
 
-            return new Tuple<bool, List<DevicesModel>>(true, aa);
+            return new Tuple<bool, List<DbDeviceModel>>(true, aa);
         }
 
         private void Init()
         {
             if (!this.InitIsDone)
             {
-                this.col = this._db.Db.GetCollection<DevicesModel>("devices");
+                this.col = this._db.Db.GetCollection<DbDeviceModel>("devices");
                 this.InitIsDone = true;
             }
         }
@@ -110,13 +113,13 @@ namespace LiIoT.Services.Db
         {
             var db = this._db.Db;
 
-            var col = db.GetCollection<DevicesModel>("devices");
+            var col = db.GetCollection<DbDeviceModel>("devices");
 
             var h2 = await col.Query().ToListAsync();
 
             this.zzDebug = "dsfdsf";
 
-            var a1 = new DevicesModel()
+            var a1 = new DbDeviceModel()
             {
                 Text = "hej2",
             };
@@ -129,16 +132,5 @@ namespace LiIoT.Services.Db
             await col.InsertAsync(a1);
             this.zzDebug = "sdfdsf";
         }
-    }
-
-    /// <summary>
-    /// Device model.
-    /// </summary>
-    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleType", Justification = "Reviewed.")]
-    public class DevicesModel
-    {
-        public ObjectId Id { get; set; }
-        public string Text { get; set; }
-
     }
 }
---
+++ src/LiIoT.Services/Devices/DeviceService.cs
@@ -0,0 +1,53 @@
+﻿// <summary>
+// Device Service.
+// </summary>
+// <copyright file="DeviceService.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
+// <author>Lennie Wennerlund (lempa)</author>
+
+namespace LiIoT.Services.Devices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using LiIoT.Services.Db;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Device service. Handling devices in software.
+    /// </summary>
+    public class DeviceService
+    {
+#pragma warning disable SA1309 // FieldNamesMustNotBeginWithUnderscore
+        private readonly ILogger<DeviceService> _logger;
+        private readonly RunDataService _rundata;
+        private readonly ConfigFileService _configfile;
+        private readonly LiteDbService _liteDb;
+        private readonly LiteDbServicesDevices _liteDbDevices;
+#pragma warning restore SA1309 // FieldNamesMustNotBeginWithUnderscore
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceService"/> class.
+        /// </summary>
+        /// <param name="logger">ILogger.</param>
+        /// <param name="rundataService">RunDataService.</param>
+        /// <param name="configFileService">ConfigFileService.</param>
+        /// <param name="liteDbService">LiteDbService.</param>
+        /// <param name="liteDbServicesDevices">LiteDbServicesDevices.</param>
+        public DeviceService(ILogger<DeviceService> logger, RunDataService rundataService, ConfigFileService configFileService, LiteDbService liteDbService, LiteDbServicesDevices liteDbServicesDevices)
+        {
+            this._logger = logger;
+            this._rundata = rundataService;
+            this._configfile = configFileService;
+            this._liteDb = liteDbService;
+            this._liteDbDevices = liteDbServicesDevices;
+            this.zzDebug = "DeviceService";
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "Reviewed.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Reviewed.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
+        private string zzDebug { get; set; }
+    }
+}
