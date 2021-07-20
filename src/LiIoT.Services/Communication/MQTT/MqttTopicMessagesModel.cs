5fb7a55adb1a3acdc3404504a10e00cfa9384896
Lennie Wennerlund <lennie@lempa.se>
2021-07-20 11:50:13 +0200

    Mqtt server models to handling topic. ref #21

---
+++ src/LiIoT.Services/Communication/MQTT/MqttTopicMessagesModel.cs
@@ -0,0 +1,45 @@
+﻿// <summary>
+// MqttTopicMessagesModel.
+// </summary>
+// <copyright file="MqttTopicMessagesModel.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
+// <author>Lennie Wennerlund (lempa)</author>
+
+namespace LiIoT.Services.Communication.MQTT
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Information about all messages that is comming thow mqtt server.
+    /// </summary>
+    public class MqttTopicMessagesModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MqttTopicMessagesModel"/> class.
+        /// </summary>
+        public MqttTopicMessagesModel()
+        {
+            this.DtAdded = DateTime.UtcNow;
+            this.ClientId = string.Empty;
+            this.Message = string.Empty;
+        }
+
+        /// <summary>
+        /// Gets or sets datetime when it as added.
+        /// </summary>
+        public DateTime DtAdded { get; set; }
+
+        /// <summary>
+        /// Gets or sets client id this message belongs to.
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// Gets or sets message from mqtt client posting.
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
